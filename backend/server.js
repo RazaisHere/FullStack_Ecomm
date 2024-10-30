96cce9e5a8f9b5db9fe53474a409d800f90fb509
Ali Raza <info678797@gmail.com>
2024-10-30 11:43:49 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,51 @@
-const express= require("express");
-const cors= require("cors")
+const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/mongoose");
 const userRouter = require("./router/userRoute");
-const cartRouter = require("./router/cartRouter")
-const adminRouter = require("./router/adminRouter")
+const cartRouter = require("./router/cartRouter");
+const adminRouter = require("./router/adminRouter");
 const productRouter = require("./router/productRouter");
 const connectCloudinary = require("./config/cloudinary");
 const adminUserRouter = require("./router/adminUserRouter");
 const orderRouter = require("./router/orderRouter");
+
 dotenv.config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000; // Corrected port assignment
 
-//config cloudinary
-connectCloudinary()
-//middlewares
+// Config Cloudinary
+connectCloudinary();
 
+// Middlewares
 app.use(express.json());
 app.use(cors({
     origin: (origin, callback) => {
-        const allowedOrigins = ["https://vercel.com/api/toolbar/link/full-stack-frontend-nine.vercel.app?via=project-dashboard-alias-list&p=1&page=/", "https://vercel.com/api/toolbar/link/full-stack-admin-pied.vercel.app?via=project-dashboard-alias-list&p=1&page=/"];
+        const allowedOrigins = [
+            "https://full-stack-frontend-nine.vercel.app",
+            "https://full-stack-admin-pied.vercel.app"
+        ];
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, origin);
         } else {
             callback(new Error('CORS policy: No access'));
         }
     },
-    credentials: true // Again, optional depending on your needs
+    credentials: true
 }));
- // Adjust to match your frontend's IP
 
-//connecting DB
-connectDB()
-//Routes
-app.use("/api/users",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/admin/",adminRouter)
-app.use("/admin/products",productRouter)
-app.use("/admin/users",adminUserRouter)
-app.use("/api/order",orderRouter)
+// Connecting DB
+connectDB();
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+// Routes
+app.use("/api/users", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/admin/", adminRouter);
+app.use("/admin/products", productRouter);
+app.use("/admin/users", adminUserRouter);
+app.use("/api/order", orderRouter);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
